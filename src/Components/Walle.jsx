22793ee8e55977634f3c210ece99154b6807bd06
Husnain Ali <128280622+husnainalyy@users.noreply.github.com>
2024-06-23 23:01:25 +0500

    Update Walle.jsx

--- src/Components/Walle.jsx
+++ src/Components/Walle.jsx
@@ -1,34 +1,27 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF, useProgress } from "@react-three/drei";
 import CanvasLoader from "../Components/Loader";
-import { Html } from "@react-three/drei";
-
-
 
 function Walle() {
     const walle = useGLTF("/walle/scene.gltf");
     const meshRef = React.useRef();
 
     return (
-        
-            <>
-                <hemisphereLight intensity={15} groundColor={0x000000} />
-                <pointLight intensity={10} />
-                <spotLight
-                    intensity={10}
-                    position={[-20, 0, 0]}
-                    angle={0.12}
-                    penumbra={1}
-                    castShadow
-                    shadow-mapSize={1024}
-                />
-                <primitive ref={meshRef} object={walle.scene} scale={3.9} position={[0, -1, 0]} rotation={[0, 4.8, 0]} />
-
-            </>
-
-        
-    )
+        <>
+            <hemisphereLight intensity={15} groundColor={0x000000} />
+            <pointLight intensity={10} />
+            <spotLight
+                intensity={10}
+                position={[-20, 0, 0]}
+                angle={0.12}
+                penumbra={1}
+                castShadow
+                shadow-mapSize={1024}
+            />
+            <primitive ref={meshRef} object={walle.scene} scale={3.9} position={[0, -1, 0]} rotation={[0, 4.8, 0]} />
+        </>
+    );
 }
 
 export const WalleCanvas = () => {
@@ -43,7 +36,7 @@ export const WalleCanvas = () => {
                 fov: 45,
                 near: 0.4,
                 far: 200,
-                position: [-20,0,0],
+                position: [-20, 0, 0],
             }}
         >
             <Suspense fallback={<CanvasLoader />}>
@@ -57,11 +50,8 @@ export const WalleCanvas = () => {
                 <Walle />
                 <Preload all />
             </Suspense>
-
         </Canvas>
-
-    )
-
+    );
 }
 
-export default Walle
+export default WalleCanvas;
